b85d20d760173e8bf8dc20347d810fc6a1a1727a
balbinfurio <xxbalbin@gmail.com>
2023-08-18 12:56:21 -0500

    ASK deleted

--- app/Http/Livewire/EmotionSelection.php
+++ app/Http/Livewire/EmotionSelection.php
@@ -6,6 +6,8 @@ use Livewire\Component;
 use App\Models\DailyEmotion;
 use App\Models\Emotion;
 use App\Models\JournalEntry;
+use \OpenAI;
+use Illuminate\Http\Request; // Agrega esta línea
 
 class EmotionSelection extends Component
 {
@@ -15,33 +17,80 @@ class EmotionSelection extends Component
 
     public function mount()
     {
-        $this->emotions = Emotion::all(); // Obtén las emociones desde tu modelo Emotion
+        $this->emotions = Emotion::all();
     }
     
-    public function saveEmotion()
+    public function saveEmotion(Request $request)
     {
-        
-        // dd('Método saveEmotion está siendo llamado');
+        // Guardar emoción y entrada en la base de datos
         $dailyEmotion = new DailyEmotion();
         $dailyEmotion->user_id = auth()->user()->id;
         $dailyEmotion->emotion_id = $this->selectedEmotion;
-
         $dailyEmotion->journal_entry = $this->writtenEntry;
-        // dd($this->writtenEntry);
-
         $dailyEmotion->save();
         
+        // Realizar la solicitud a la API de OpenAI
+        $yourApiKey = "";
+        $client = OpenAI::client($yourApiKey);
         
-        $this->reset(['selectedEmotion']);
-        // $this->reset(['writtenEntry']);
+        $result = $client->completions()->create([
+            'model' => 'text-davinci-003',
+            'prompt' => $this->writtenEntry,
+            'max_tokens' => 1000
+        ]);
 
+        // Aquí podrías guardar la respuesta de OpenAI en una base de datos si deseas
+        // $journalEntry = new JournalEntry();
+        // $journalEntry->user_id = auth()->user()->id;
+        // $journalEntry->response_from_openai = $result['choices'][0]['text'];
+        // $journalEntry->save();
+        
+        $this->reset(['selectedEmotion', 'writtenEntry']);
+        
         session()->flash('message', 'Registros guardados exitosamente.');
-
     }
-
-
+    
     public function render()
     {
         return view('livewire.emotion-selection');
     }
 }
+
+
+// class EmotionSelection extends Component
+// {
+//     public $selectedEmotion;
+//     public $emotions;
+//     public $writtenEntry;
+
+//     public function mount()
+//     {
+//         $this->emotions = Emotion::all(); // Obtén las emociones desde tu modelo Emotion
+//     }
+    
+//     public function saveEmotion()
+//     {
+        
+//         // dd('Método saveEmotion está siendo llamado');
+//         $dailyEmotion = new DailyEmotion();
+//         $dailyEmotion->user_id = auth()->user()->id;
+//         $dailyEmotion->emotion_id = $this->selectedEmotion;
+//         $dailyEmotion->journal_entry = $this->writtenEntry;
+//         // dd($this->writtenEntry);
+
+//         $dailyEmotion->save();
+        
+        
+//         $this->reset(['selectedEmotion']);
+//         // $this->reset(['writtenEntry']);
+
+//         session()->flash('message', 'Registros guardados exitosamente.');
+
+//     }
+
+
+//     public function render()
+//     {
+//         return view('livewire.emotion-selection');
+//     }
+// }
