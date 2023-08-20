dbb13130e039d3e7f79c6ce34cec64fdebe2fb52
balbinfurio <xxbalbin@gmail.com>
2023-08-19 19:58:06 -0500

    almost done 95%

--- app/Http/Livewire/EmotionSelection.php
+++ app/Http/Livewire/EmotionSelection.php
@@ -7,13 +7,15 @@ use App\Models\DailyEmotion;
 use App\Models\Emotion;
 use App\Models\JournalEntry;
 use \OpenAI;
-use Illuminate\Http\Request; // Agrega esta línea
+use Illuminate\Http\Request;
 
 class EmotionSelection extends Component
 {
     public $selectedEmotion;
     public $emotions;
     public $writtenEntry;
+    public $openAIResponse;
+
 
     public function mount()
     {
@@ -22,75 +24,56 @@ class EmotionSelection extends Component
     
     public function saveEmotion(Request $request)
     {
+        // Verificar si ya existe un registro para el usuario en el día actual
+        $dailyEmotion = DailyEmotion::where('user_id', auth()->user()->id)
+        ->whereDate('created_at', now()->format('Y-m-d'))
+        ->first();
+
+        if ($dailyEmotion) {
+            return redirect()->back()->with('error', 'You have already registered your thoughts today.');
+        }
+
         // Guardar emoción y entrada en la base de datos
         $dailyEmotion = new DailyEmotion();
         $dailyEmotion->user_id = auth()->user()->id;
         $dailyEmotion->emotion_id = $this->selectedEmotion;
         $dailyEmotion->journal_entry = $this->writtenEntry;
-        $dailyEmotion->save();
         
         // Realizar la solicitud a la API de OpenAI
-        // $yourApiKey = "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";
-        // $client = OpenAI::client($yourApiKey);
+        $yourApiKey = env('OPENAI_API_KEY');
+        $client = OpenAI::client($yourApiKey);
         
-        // $result = $client->completions()->create([
-        //     'model' => 'text-davinci-003',
-        //     'prompt' => $this->writtenEntry,
-        //     'max_tokens' => 5
-        // ]);
-
-        // Aquí podrías guardar la respuesta de OpenAI en una base de datos si deseas
-        // $journalEntry = new JournalEntry();
-        // $journalEntry->user_id = auth()->user()->id;
-        // $journalEntry->response_from_openai = $result['choices'][0]['text'];
-        // $journalEntry->save();
+        $result = $client->completions()->create([
+            'model' => 'text-davinci-003',
+            'prompt' => $this->writtenEntry,
+            'max_tokens' => 50
+        ]);
+        
+        $dailyEmotion->OpenAI = $result['choices'][0]['text'];
+        $dailyEmotion->save();
         
         $this->reset(['selectedEmotion', 'writtenEntry']);
         
-        session()->flash('message', 'Registros guardados exitosamente.');
+        session()->flash('message', 'Your thoughts have been saved.');
+    }
+
+    public function getOpenAI()
+    {
+        $dailyEmotion = DailyEmotion::where('user_id', auth()->user()->id)
+            ->whereDate('created_at', now()->toDateString())
+            ->first();
+            
+        
+        if ($dailyEmotion) {
+            $this->openAIResponse = $dailyEmotion->OpenAI;
+        }
+    
+        // dd($this->openAIResponse);
+
     }
     
     public function render()
     {
-        return view('livewire.emotion-selection');
+        return view('livewire.emotion-selection', );
     }
 }
-
-
-// class EmotionSelection extends Component
-// {
-//     public $selectedEmotion;
-//     public $emotions;
-//     public $writtenEntry;
-
-//     public function mount()
-//     {
-//         $this->emotions = Emotion::all(); // Obtén las emociones desde tu modelo Emotion
-//     }
-    
-//     public function saveEmotion()
-//     {
-        
-//         // dd('Método saveEmotion está siendo llamado');
-//         $dailyEmotion = new DailyEmotion();
-//         $dailyEmotion->user_id = auth()->user()->id;
-//         $dailyEmotion->emotion_id = $this->selectedEmotion;
-//         $dailyEmotion->journal_entry = $this->writtenEntry;
-//         // dd($this->writtenEntry);
-
-//         $dailyEmotion->save();
-        
-        
-//         $this->reset(['selectedEmotion']);
-//         // $this->reset(['writtenEntry']);
-
-//         session()->flash('message', 'Registros guardados exitosamente.');
-
-//     }
-
-
-//     public function render()
-//     {
-//         return view('livewire.emotion-selection');
-//     }
-// }
--- resources/views/livewire/emotion-selection.blade.php
+++ resources/views/livewire/emotion-selection.blade.php
@@ -1,29 +1,39 @@
 <div>
-    @if(session()->has('message'))
-        <div>{{ session('message') }}</div>
+    @if(session()->has('error'))
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
     @endif
 
+    @if(session()->has('message'))
+        <div class="alert alert-success">
+            {{ session('message') }}
+        </div>
+    @endif
+
+
     <div class="flex justify-center items-center">
-        <div id="dash-first">
-            <h2 class="text-3xl font-semibold text-center mb-5">HOW DO YOU FEEL TODAY?</h2>
+        <div id="dash-first" class="text-center">
+            <h2 class="text-3xl font-semibold mb-5">HOW DO YOU FEEL TODAY?</h2>
             <form wire:submit.prevent="saveEmotion">
                 @csrf
-                <div class="flex gap-4">
-                    <button type="button" wire:click="$set('selectedEmotion', 1)" class="text-9xl @if($selectedEmotion === 1) selected-emoji @endif" title="HAPPY">😀</button>
-                    <button type="button" wire:click="$set('selectedEmotion', 2)" class="text-9xl @if($selectedEmotion === 2) selected-emoji @endif" title="SAD">😞</button>
-                    <button type="button" wire:click="$set('selectedEmotion', 3)" class="text-9xl @if($selectedEmotion === 3) selected-emoji @endif" title="ANGRY">😡</button>
-                    <button type="button" wire:click="$set('selectedEmotion', 4)" class="text-9xl @if($selectedEmotion === 4) selected-emoji @endif" title="CALM">🙂</button>
-                    <button type="button" wire:click="$set('selectedEmotion', 5)" class="text-9xl @if($selectedEmotion === 5) selected-emoji @endif" title="EXCITED">😏</button>
+                <div class="flex flex-col md:flex-row gap-4">
+                    <button type="button" wire:click="$set('selectedEmotion', 1)" class="text-6xl md:text-9xl @if($selectedEmotion === 1) selected-emoji @endif" title="HAPPY">😀</button>
+                    <button type="button" wire:click="$set('selectedEmotion', 2)" class="text-6xl md:text-9xl @if($selectedEmotion === 2) selected-emoji @endif" title="SAD">😞</button>
+                    <button type="button" wire:click="$set('selectedEmotion', 3)" class="text-6xl md:text-9xl @if($selectedEmotion === 3) selected-emoji @endif" title="ANGRY">😡</button>
+                    <button type="button" wire:click="$set('selectedEmotion', 4)" class="text-6xl md:text-9xl @if($selectedEmotion === 4) selected-emoji @endif" title="CALM">🙂</button>
+                    <button type="button" wire:click="$set('selectedEmotion', 5)" class="text-6xl md:text-9xl @if($selectedEmotion === 5) selected-emoji @endif" title="EXCITED">😏</button>
                 </div>
             </form>
         </div>
     </div>
     
     
+    
     <div class="flex justify-center items-center">
         <div id="dash-sec">
             <div>
-                <h2 class="text-3xl font-semibold text-center mb-5">SHOW ME YOUR THOUGHS</h2>
+                <h2 class="text-3xl font-semibold text-center mb-5">SHOW ME YOUR THOUGHTS</h2>
                 <form wire:submit.prevent="saveEmotion">
                     <textarea id="text-entry" class="w-96 h-48 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 placeholder-gray-400" 
                         wire:model="writtenEntry" 
@@ -34,9 +44,28 @@
                     <button type="submit" id="submitEmotion" class="px-6 py-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg shadow-md transition duration-300 ease-in-out">
                         SAVE
                     </button>
+
+                    
                     
                 </form>
             </div>
         </div>
     </div>
+
+    <div class="flex justify-center items-center">
+        <div id="dash-third">
+            <form wire:submit.prevent="getOpenAI">
+                <!-- Otros campos aquí -->
+                <button type="submit" id="submitEmotion" class="px-6 py-2 bg-green-500 hover:bg-green-600 text-white font-semibold rounded-lg shadow-md transition duration-300 ease-in-out">
+                    CLICK FOR RESPONSE
+                </button>
+            </form>
+            @if ($openAIResponse)
+                <div id="openAI-response" class="bg-green-300 p-4 rounded-lg">
+                    <p>{{ $openAIResponse }}</p>
+                </div>
+            @endif
+        </div>
+    </div>
+    
 </div>
